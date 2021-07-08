37383da95369d4b2e78b8ae59e3ccc0947fcc967
chlrhalsgur <66667677+chlrhalsgur@users.noreply.github.com>
2021-07-08 14:24:44 +0900

    Add files via upload

--- Rerating_movie.py
+++ Rerating_movie.py
@@ -1,4 +1,4 @@
-import db_test1
+import database
 
 DRAMA_FACTOR = 0.98
 ANIMATION_FACTOR = 0.98
@@ -20,13 +20,17 @@ NOIR_FACTOR = 1.04
 NONE = 1.0
 
 factors = [DRAMA_FACTOR, FANTASY_FACTOR, NONE, HORROR_FACTOR, ROMANCE_FACTOR, ADVENTURE_FACTOR, THRILLER_FACTOR, NOIR_FACTOR, NONE, DOCUMENTARY_FACTOR, COMEDY_FACTOR, FAMILY_FACTOR, MYSTERY_FACTOR, WAR_FACTOR, ANIMATION_FACTOR, CRIMINAL_FACTOR, MUSICAL_FACTOR, SF_FACTOR, ACTION_FACTOR]
-# print(len(factors))
+
+'''
+영화 재평가. 
+가중치 factors를 곱함. 영화 페이지에 3, 9페이지 없어서 제외함
+'''
 def rerate(movie_list):
     for movie in movie_list:
         if movie[2] == 3 or movie[2] == 9:
             continue
         movie[1] = str(float(movie[1]) * factors[movie[2]-1])
-        # print(movie)
-        db_test1.input_db('movie_list', 'rerated_movie_table', movie[0],movie[1],movie[2])
-    
+
+        database.input_db('movie_list', 'rerated_movie_table', movie[0],movie[1],movie[2], movie[3])
+        
 
